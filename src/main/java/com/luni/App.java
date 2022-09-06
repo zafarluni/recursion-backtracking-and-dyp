6c9ae9df2b90a71c0c268f824acfe53ee75c7a06
Replit user <>
2022-09-06 19:35:35 +0000

    made changes

--- src/main/java/com/luni/App.java
+++ src/main/java/com/luni/App.java
@@ -1,9 +1,12 @@
 package com.luni;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.luni.recursion.*;
+
+public class App {
+	public static void main(String[] args) {
+		Fibonacci fib = new Fibonacci();
+		System.out.println("Fib: "+ fib.fibonacciTail(7));
+		System.out.println("Fib: "+ fib.fibonacciTail(8));
+		System.out.println("Fib: "+ fib.fibonacciTail(9));
+	}
 }
---
+++ src/main/java/com/luni/recursion/Fibonacci.java
@@ -0,0 +1,18 @@
+package com.luni.recursion;
+
+public final class Fibonacci {
+
+	public int fibonacciTail(int number) {
+		return fibTailRec(number, 0, 1);
+	}
+
+	private int fibTailRec(int number, int a, int b) {
+		if (number == 0)
+			return a;
+		if (number == 1)
+			return b;
+
+		return fibTailRec(number - 1, b, a + b);
+		
+	}
+}
